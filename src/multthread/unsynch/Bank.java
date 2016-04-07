c9d39375982c7cb603a192880d8591fe400edd6b
dsz <364615401@qq.com>
2016-04-07 22:44:13 +0800

    +1

--- src/multthread/unsynch/Bank.java
+++ src/multthread/unsynch/Bank.java
@@ -1,28 +1,33 @@
 package multthread.unsynch;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
+		
 		private final double[]accounts;
 		private Lock lock=new ReentrantLock();
+		
+		private Condition sufficientFunds;
 		public Bank(int n,double initialBalance){
 			accounts=new double[n];
+			sufficientFunds=lock.newCondition();
 			for(int i=0;i<accounts.length;i++){
 				accounts[i]=initialBalance;
 			}
 		}
 		
-	public void  transfer(int from ,int to,double amount){
+	public void  transfer(int from ,int to,double amount) throws InterruptedException{
 			lock.lock();
 			try{
-				if(accounts[from]<amount) return;
+				while (accounts[from]<amount) sufficientFunds.await();;
 				System.out.println(Thread.currentThread());
-				
 				accounts[from]-=amount;
 				System.out.printf("%10.2f from %d to %d",amount,from,to);
 				accounts[to]+=amount;
 				System.out.printf("Total Balance :%10.2f%n",getTotalBalance());
+				sufficientFunds.signalAll();
 			}finally{
 				lock.unlock();
 				
@@ -32,12 +37,17 @@ public class Bank {
 		}
 
 		public double getTotalBalance() {
-			double sum=0;
-			for(double a:accounts){
-				sum+=a;
+			try{
+				lock.lock();
+				double sum=0;
+				for(double a:accounts){
+					sum+=a;
+				}
+				return sum;	
+			}finally{
+				lock.unlock();
 			}
 			
-			return sum;
 		}
 		public int size(){
 			
