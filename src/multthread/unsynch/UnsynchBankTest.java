b8fe0618c9cfe60346f310fc32ba2c58074de7d0
dsz <364615401@qq.com>
2016-04-07 20:52:11 +0800

    多线程

--- src/multthread/unsynch/UnsynchBankTest.java
+++ src/multthread/unsynch/UnsynchBankTest.java
@@ -1,5 +1,9 @@
 package multthread.unsynch;
 
 public class UnsynchBankTest {
-	
+	public static final int NACCOUNT=100;
+	public static final double INTIAL_BALANCE=1000;
+	public static void main(String[] args) {
+		 
+	}
 }
